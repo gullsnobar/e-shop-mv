a95f9703864f78df38658d3f06c398468c2a1c54
Gull Snobar <abdulrehmanali7775@gmail.com>
2026-04-18 23:14:55 +0500

    Bugs are fixed

--- backend/model/user.js
+++ backend/model/user.js
@@ -6,15 +6,19 @@ const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, "Please enter your name!"],
+    trim: true,
   },
   email: {
     type: String,
     required: [true, "Please enter your email!"],
+    unique: true,
+    lowercase: true,
+    trim: true,
   },
   password: {
     type: String,
     required: [true, "Please enter your password"],
-    minLength: [4, "Password should be greater than 4 characters"],
+    minlength: [4, "Password should be greater than 4 characters"],
     select: false,
   },
   phoneNumber: {
@@ -22,24 +26,12 @@ const userSchema = new mongoose.Schema({
   },
   addresses: [
     {
-      country: {
-        type: String,
-      },
-      city: {
-        type: String,
-      },
-      address1: {
-        type: String,
-      },
-      address2: {
-        type: String,
-      },
-      zipCode: {
-        type: Number,
-      },
-      addressType: {
-        type: String,
-      },
+      country: String,
+      city: String,
+      address1: String,
+      address2: String,
+      zipCode: Number,
+      addressType: String,
     },
   ],
   role: {
@@ -58,29 +50,27 @@ const userSchema = new mongoose.Schema({
   },
   createdAt: {
     type: Date,
-    default: Date.now(),
+    default: Date.now,   // ✅ Fixed: Date.now (not Date.now())
   },
   resetPasswordToken: String,
   resetPasswordTime: Date,
 });
 
-//  Hash password
+// Hash password
 userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) {
-    next();
-  }
-
+  if (!this.isModified("password")) return next();
   this.password = await bcrypt.hash(this.password, 10);
+  next();
 });
 
-// jwt token
+// JWT token
 userSchema.methods.getJwtToken = function () {
   return jwt.sign({ id: this._id }, process.env.JWT_SECRET_KEY, {
     expiresIn: process.env.JWT_EXPIRES,
   });
 };
 
-// compare password
+// Compare password
 userSchema.methods.comparePassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
