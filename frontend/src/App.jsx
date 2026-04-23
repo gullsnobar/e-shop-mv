a7d4ca0468b68d88dd1878aa6ab5a6791336cc19
Gull Snobar <abdulrehmanali7775@gmail.com>
2026-04-23 12:51:10 +0500

    working on activation and jwt tokens

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/Login.jsx";
 import SignupPage from "./pages/Signup.jsx";
+import { ActivationPage } from "./Routes.js";
 import "./App.css";
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
           <Route path="/" element={<LoginPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
+          <Route path="/activation/activation-token" element={<ActivationPage />} />
         </Routes>
       </BrowserRouter>
     </div>
