5770f2c2ca10a10217e91d66746e9790f5cd6a81
Gull Snobar <abdulrehmanali7775@gmail.com>
2026-04-25 15:16:42 +0500

    Header part is complete

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,9 +1,11 @@
 import LoginPage from "./pages/Login";
 import SignupPage from "./pages/Signup";
 import ActivationPage from "./pages/ActivationPage";
+import HomePage from "./pages/HomePage"
 
 
 export { 
+    HomePage,
     LoginPage,
     SignupPage,
     ActivationPage
