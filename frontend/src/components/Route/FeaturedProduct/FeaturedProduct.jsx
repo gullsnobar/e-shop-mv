516e6355eec0b4c841dcbe6bc7a2b985a73db2bf
Gull Snobar <abdulrehmanali7775@gmail.com>
2026-05-12 18:10:04 +0500

    add new feature

--- frontend/src/components/Route/FeaturedProduct/FeaturedProduct.jsx
+++ frontend/src/components/Route/FeaturedProduct/FeaturedProduct.jsx
@@ -1,10 +1,16 @@
 import React, { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import styles from "../../../styles/styles";
 import ProductCard from "../ProductCard/ProductCard";
+import { getAllProducts } from "../../../redux/actions/products";
 
 const FeaturedProduct = () => {
   const {allProducts} = useSelector((state) => state.products);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAllProducts());
+  }, [dispatch]);
    
   return (
     <div>
@@ -12,14 +18,14 @@ const FeaturedProduct = () => {
         <div className={`${styles.heading}`}>
           <h1>Featured Products</h1>
         </div>
-        <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12 border-0">
-        {
-            allProducts && allProducts.length !== 0 &&(
-              <>
-               {allProducts && allProducts.map((i, index) => <ProductCard data={i} key={index} />)}
-              </>
-            )
-           }
+        <div className="w-full grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12">
+          {allProducts && allProducts.length > 0 ? (
+            allProducts.map((i, index) => <ProductCard data={i} key={i._id || i.id || index} />)
+          ) : (
+            <div className="col-span-full text-center py-12">
+              <p className="text-gray-500">No products available at the moment.</p>
+            </div>
+          )}
         </div>
       </div>
     </div>
